173a99b5510918e1cda7f62d5eac012e3ae23840
adilet berdibekov <berdibekovadilet@gmail.com>
2022-10-30 15:59:28 +0600

    work with numbers in useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { useState } from "react";
 import "./styles/App.css";
 
 function App() {
-  return <div>123</div>;
+  const [likes, setLikes] = useState(0);
+
+  return (
+    <div className="App">
+      <div>{likes}</div>
+      <button onClick={() => setLikes(likes + 1)}>Increment</button>
+      <button onClick={() => setLikes(likes - 1)}>Decrement</button>
+    </div>
+  );
 }
 
 export default App;
