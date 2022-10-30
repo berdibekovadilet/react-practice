9e83c0f82536909d951657504da46b9ac37b286a
adilet berdibekov <berdibekovadilet@gmail.com>
2022-10-30 23:15:41 +0600

    added Form and setPost

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 import "./styles/App.css";
 
 function App() {
@@ -20,8 +22,33 @@ function App() {
       body: "JavaScript — мультипарадигменный язык программирования. Поддерживает объектно-ориентированный, императивный и функциональный стили.",
     },
   ]);
+
+  const [post, setPost] = useState({ title: "", body: "" });
+
+  const addNewPost = (e) => {
+    e.preventDefault();
+
+    setPosts([...posts, { ...post, id: Date.now() }]);
+    setPost({ title: "", body: "" });
+  };
+
   return (
     <div className="App">
+      <form>
+        <MyInput
+          value={post.title}
+          onChange={(e) => setPost({ ...post, title: e.target.value })}
+          type="text"
+          placeholder="Заголовок поста"
+        />
+        <MyInput
+          value={post.body}
+          onChange={(e) => setPost({ ...post, body: e.target.value })}
+          type="text"
+          placeholder="Описание поста"
+        />
+        <MyButton onClick={addNewPost}>Создать</MyButton>
+      </form>
       <PostList posts={posts} title="Список Постов" />
     </div>
   );
