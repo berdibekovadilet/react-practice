5bb25f720e869207e9851122b12ea0a931933f85
adilet berdibekov <berdibekovadilet@gmail.com>
2022-10-30 17:33:06 +0600

    change PostList

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { useState } from "react";
 import PostList from "./components/PostList";
 import "./styles/App.css";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: "JavaScript",
+      body: "JavaScript — мультипарадигменный язык программирования. Поддерживает объектно-ориентированный, императивный и функциональный стили.",
+    },
+    {
+      id: 2,
+      title: "Phyton",
+      body: "JavaScript — мультипарадигменный язык программирования. Поддерживает объектно-ориентированный, императивный и функциональный стили.",
+    },
+    {
+      id: 3,
+      title: "Php",
+      body: "JavaScript — мультипарадигменный язык программирования. Поддерживает объектно-ориентированный, императивный и функциональный стили.",
+    },
+  ]);
   return (
     <div className="App">
-      <PostList />
+      <PostList posts={posts} />
     </div>
   );
 }
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,27 +1,8 @@
-import { useState } from "react";
 import PostItem from "./PostItem";
 
-const PostList = () => {
-  const [posts, setPosts] = useState([
-    {
-      id: 1,
-      title: "JavaScript",
-      body: "JavaScript — мультипарадигменный язык программирования. Поддерживает объектно-ориентированный, императивный и функциональный стили.",
-    },
-    {
-      id: 2,
-      title: "Phyton",
-      body: "JavaScript — мультипарадигменный язык программирования. Поддерживает объектно-ориентированный, императивный и функциональный стили.",
-    },
-    {
-      id: 3,
-      title: "Php",
-      body: "JavaScript — мультипарадигменный язык программирования. Поддерживает объектно-ориентированный, императивный и функциональный стили.",
-    },
-  ]);
+const PostList = ({ posts }) => {
   return (
     <div>
-      {" "}
       <h1 style={{ textAlign: "center" }}>Список Постов</h1>
       {posts.map((post) => (
         <PostItem post={post} key={post.id} />
