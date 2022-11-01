80275d65f8973ba51d0a4f811a0495a659c5a95a
Adilet Berdibekov <40534332+berdibekovadilet@users.noreply.github.com>
2022-11-01 19:09:40 +0600

    added routes

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,26 +1,19 @@
 import React from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
-import About from "../pages/About";
-import Error from "../pages/Error";
-import PostIdPage from "../pages/PostIdPage";
-import Posts from "../pages/Posts";
+import { privateRoutes } from "../router";
 
 const AppRouter = () => {
   return (
     <Switch>
-      <Route exact path="/posts">
-        <Posts />
-      </Route>
-      <Route exact path="/posts/:id">
-        <PostIdPage />
-      </Route>
-      <Route path="/about">
-        <About />
-      </Route>
-      <Route path="/error">
-        <Error />
-      </Route>
-      <Redirect to="/error" />
+      {privateRoutes.map((route) => (
+        <Route
+          component={route.component}
+          path={route.path}
+          exact={route.exact}
+          key={route.path}
+        />
+      ))}
+      <Redirect to="/posts" />
     </Switch>
   );
 };
