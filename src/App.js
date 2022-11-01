039cd86eb3cb5228bf7d20992ce855ba50756947
Adilet Berdibekov <40534332+berdibekovadilet@users.noreply.github.com>
2022-11-01 17:09:15 +0600

    added 404 page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import Navbar from "./components/UI/navbar/Navbar";
+import About from "./pages/About";
+import Error from "./pages/Error";
 import Posts from "./pages/Posts";
 import "./styles/App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Posts />
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <div className="App">
+        <Switch>
+          <Route path="/posts">
+            <Posts />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/error">
+            <Error />
+          </Route>
+          <Redirect to="/error" />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
