c2a3b1c01b1c3e4241d40ea476d95255fff71aa3
Adilet Berdibekov <40534332+berdibekovadilet@users.noreply.github.com>
2022-11-01 18:45:22 +0600

    added open page by id

--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import PostService from "../API/PostService";
+import Loader from "../components/UI/loader/Loader";
+import { useFetching } from "../hooks/useFetching";
 
 const PostIdPage = () => {
   const params = useParams();
+  const [post, setPost] = useState({});
+  const [fetchPostById, isLoading, error] = useFetching(async (id) => {
+    const response = await PostService.getById(id);
+    setPost(response.data);
+  });
+
+  useEffect(() => {
+    fetchPostById(params.id);
+  }, []);
+
   return (
-    <div>
-      <h1>Вы открыли страницу поста c ID = {params.id}</h1>
+    <div className="page" style={{ marginTop: 50 }}>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div>
+          <h2>
+            {post.id}. {post.title}
+          </h2>
+          <div>{post.body}</div>
+        </div>
+      )}
     </div>
   );
 };
